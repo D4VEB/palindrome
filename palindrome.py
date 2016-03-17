9b26bd924d901ec5eae097de9f63726fdae3ef4f
D4VEB <dhblodgett@gmail.com>
2016-03-17 07:30:29 -0700

    Normal Mode (no logging)

--- palindrome.py
+++ palindrome.py
@@ -1,2 +1,23 @@
+import re
+
+def strip_text(text):
+     match = re.sub(r"\W", "", text)
+     stripped = match.lower()
+     return stripped
+
+
+def palindrome_test(backward, forward, word):
+    if backward <= forward:
+        return True
+    if word[backward] != word[forward]:
+        return False
+    else:
+        return palindrome_test(backward - 1, forward + 1, word)
+
 def palindrome(string):
-    return False
+
+    string_to_test = strip_text(string)
+    forward = 0
+    backward = len(string_to_test) - 1
+
+    return palindrome_test(backward, forward, string_to_test)
